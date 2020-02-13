e722570756d2545c1228938d13af5cf11242712d
karthikeyanmp <karthikeyanmpme@gmail.com>
2020-02-13 14:51:50 +0530

    level4 Assignment - first submission

---
+++ todo.rb
@@ -0,0 +1,53 @@
+class Todo < ActiveRecord::Base
+  def due_today?
+    due_date == Date.today
+  end
+
+  def overdue?
+    due_date < Date.today
+  end
+
+  def due_later?
+    due_date > Date.today
+  end
+
+  def to_displayable_string
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today? ? nil : due_date
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
+  end
+
+  def self.add_task(task)
+    create!(todo_text: task[:todo_text], due_date: Date.today + task[:due_in_days], completed: false)
+  end
+
+  def self.to_displayable_list
+    all.map { |todo| todo.to_displayable_string }
+  end
+
+  def self.show_list
+    puts "My Todo-list"
+    puts "\n"
+    puts "Overdue"
+    all.order(id: :asc).select do |todo|
+      puts todo.to_displayable_string if todo.overdue?
+    end
+    puts "\n"
+    puts "Due Today"
+    all.order(id: :asc).select do |todo|
+      puts todo.to_displayable_string if todo.due_today?
+    end
+    puts "\n"
+    puts "Due Later"
+    all.order(id: :asc).select do |todo|
+      puts todo.to_displayable_string if todo.due_later?
+    end
+  end
+
+  def self.mark_as_complete!(todo_id)
+    todo = find(todo_id)
+    todo.completed = true
+    todo.save
+    todo
+  end
+end
